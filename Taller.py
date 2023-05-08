98f4fbf5f9f3b53634a60be0a56b794b57f37fc9
JavAlex17 <72239869+JavAlex17@users.noreply.github.com>
2023-05-08 11:10:26 -0400

    VentanaComparacion

--- Taller.py
+++ Taller.py
@@ -2,3 +2,32 @@ import sys
 import pandas as pd
 from PyQt6.QtWidgets import QApplication, QMainWindow, QWidget, QTableWidget, QTableWidgetItem, QVBoxLayout, QHBoxLayout, QPushButton
 
+#Yen vs Dolar Can.
+#
+
+class VentanaComparacion(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.resize(1000,500)
+        
+        self.setWindowTitle("Grupo 4: Yen vs Dólar Canadience")
+        
+        self.botonComparar = QPushButton('Comparar')
+        self.botonComparar.clicked.connect(self.comparar)
+        layout = QVBoxLayout()
+        layout.addWidget(self.botonComparar)
+        central_widget = QWidget()
+        central_widget.setLayout(layout)
+        self.setCentralWidget(central_widget)
+        
+    def comparar():
+        pass
+        
+
+    
+    
+app = QApplication(sys.argv)
+ventana = VentanaComparacion()
+ventana.show()
+sys.exit(app.exec())    
+        
