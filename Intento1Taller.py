88f1fee1f99292a5ab5d1220320d17a707252bf3
pipelinioo <felipeignacio.delgado@alumnos.ulagos.cl>
2023-05-08 12:40:04 -0400

    prueba 4

--- Intento1Taller.py
+++ Intento1Taller.py
@@ -1,107 +1,135 @@
 import sys
-from PyQt6.QtWidgets import QApplication, QMainWindow, QWidget, QLabel, QTableWidget, QTableWidgetItem, QVBoxLayout, QHBoxLayout, QPushButton, QLineEdit, QFormLayout, QDialog
-#Yen vs Dolar Can.
-# 
+from PyQt6.QtWidgets import QApplication, QWidget, QVBoxLayout, QHBoxLayout, QLabel, QLineEdit, QPushButton, QDialog, QFormLayout
 
-class VentanaComparacion(QMainWindow):
-    def __init__(self):
-        super().__init__()
-        self.resize(600,500)
-        self.setWindowTitle("Grupo 4: Yen vs Dólar Canadience")
-        layout = QVBoxLayout()
-        
-        messageL = QLabel('Bienvenido a la aplicación para comparar Divisas')
-        layout.addWidget(messageL)
-        
-        
-        central_widget = QWidget()
-        central_widget.setLayout(layout)
-        self.setCentralWidget(central_widget)
-        
-        
-        self.botonElementoIzq = QPushButton('Agregar Atributos')
-        self.botonElementoIzq.clicked.connect(self.elementoIzq)
-        layout.addWidget(self.botonElementoIzq)
-        #layout.addWidget(self.botonElementoIzq)
-        
-        self.botonElementoDer = QPushButton('Agregar Atributos')
-        self.botonElementoDer.clicked.connect(self.elementoDer)
-        layout.addWidget(self.botonElementoDer)
-        
-        izq_widget = QWidget()
-        izq_widget.setLayout(layout)
-        self.setCentralWidget(izq_widget)
-        
-        self.botonComparar = QPushButton('Comparar')
-        self.botonComparar.clicked.connect(self.comparar)
-        layout.addWidget(self.botonComparar)
-        
-        
-        
-    def elementoIzq(self):
-        
-        pass
-    
-    def elementoDer(self):
-        pass
-        
-        
-    def comparar():
-        pass
 
-        
 class Divisa:
-    def __init__(self,nombredivisa,valordivisa):
-        self.nombredivisa = nombredivisa
-        self.valordivisa = float(valordivisa)
+    def __init__(self, nombre, valordivisa):
+        self.nombre = nombre
+        self.valordivisa = valordivisa
+
+    def calcular_equivalente_usd(self):
+        if self.nombre == "Yen Japones":
+            return self.valordivisa / 134.80
+        elif self.nombre == "Dolar Canadiense":
+            return self.valordivisa / 1.3371
 
 
-class VentanaComparacion(QMainWindow):
+class DatosDivisaDialog(QDialog):
+    def __init__(self, divisa):
+        super().__init__()
+        self.setWindowTitle("Ingresar Datos de Divisa")
+        self.divisa = divisa
+        self.setup_ui()
+
+    def setup_ui(self):
+        layout = QVBoxLayout()
+
+        form_layout = QFormLayout()
+
+        self.input_nombre = QLineEdit()
+        self.input_nombre.setText(self.divisa.nombre)
+        form_layout.addRow("Nombre:", self.input_nombre)
+
+        self.input_valordivisa = QLineEdit()
+        self.input_valordivisa.setText(str(self.divisa.valordivisa))
+        form_layout.addRow("Valor Divisa:", self.input_valordivisa)
+
+        layout.addLayout(form_layout)
+
+        btn_guardar = QPushButton("Guardar")
+        btn_guardar.clicked.connect(self.guardar_datos)
+        layout.addWidget(btn_guardar)
+
+        self.setLayout(layout)
+
+    def guardar_datos(self):
+        self.divisa.nombre = self.input_nombre.text()
+        self.divisa.valordivisa = float(self.input_valordivisa.text())
+        self.accept()
+
+
+class GUI(QWidget):
     def __init__(self):
         super().__init__()
-        self.resize(600,500)
-        
-        self.setWindowTitle("Grupo 4: Yen vs Dólar Canadience")
-        
-        self.botonComparar = QPushButton('Comparar')
-        self.botonComparar.clicked.connect(self.comparar)
+        self.setWindowTitle("Comparación de Divisas")
+        self.divisa_izquierda = Divisa("Yen Japones", 0.0)
+        self.divisa_derecha = Divisa("Dolar Canadiense", 0.0)
+        self.setup_ui()
+
+    def setup_ui(self):
         layout = QVBoxLayout()
-        layout.addWidget(self.botonComparar)
-        central_widget = QWidget()
-        central_widget.setLayout(layout)
-        self.setCentralWidget(central_widget)
 
-    def comparar(self):
-        # Creamos una instancia de la ventana secundaria y la mostramos
-        ventanaDatos = QDialog()
-        ventanaDatos.setWindowTitle("Ingresar nueva divisa")
-        etiquetaNombre = QLabel("Nombre de la divisa:")
-        entradaNombre = QLineEdit()
-        etiquetaValor = QLabel("Valor de la divisa:")
-        entradaValor = QLineEdit()
-        botonAceptar = QPushButton("Aceptar")
-        botonAceptar.clicked.connect(ventanaDatos.accept)
-        botonCancelar = QPushButton("Cancelar")
-        botonCancelar.clicked.connect(ventanaDatos.reject)
+        # Texto de bienvenida
+        label_bienvenida = QLabel("¡Bienvenido! Ingresa los datos de las divisas:")
+        layout.addWidget(label_bienvenida)
 
-        layout = QVBoxLayout()
-        layout.addWidget(etiquetaNombre)
-        layout.addWidget(entradaNombre)
-        layout.addWidget(etiquetaValor)
-        layout.addWidget(entradaValor)
-        layout.addWidget(botonAceptar)
-        layout.addWidget(botonCancelar)
+        # Texto de instrucciones
+        label_instrucciones = QLabel("Ingrese el valor de la divisa en su respectiva moneda:")
+        layout.addWidget(label_instrucciones)
 
-        ventanaDatos.setLayout(layout)
+        # Panel de comparación
+        panel_comparacion = QHBoxLayout()
 
-        if ventanaDatos.exec() == QDialog.Accepted:
-            nombreDivisa = entradaNombre.text()
-            valorDivisa = entradaValor.text()
-            divisa = Divisa(nombreDivisa, valorDivisa)
-            print(f'Se ingresó una nueva divisa: {divisa.nombredivisa} = {divisa.valordivisa}')
-    
-app = QApplication(sys.argv)
-ventana = VentanaComparacion()
-ventana.show()
-sys.exit(app.exec())    
+        # Despliegue de información del objeto a la izquierda
+        self.label_izquierda = QLabel()
+        panel_comparacion.addWidget(self.label_izquierda)
+
+        # Texto central que indica quién es mayor
+        self.label_comparacion = QLabel()
+        panel_comparacion.addWidget(self.label_comparacion)
+
+        # Despliegue de información del objeto a la derecha
+        self.label_derecha = QLabel()
+        panel_comparacion.addWidget(self.label_derecha)
+
+        layout.addLayout(panel_comparacion)
+
+        # Botón de actualización de comparación
+        btn_actualizar = QPushButton("Actualizar Comparación")
+        btn_actualizar.clicked.connect(self.actualizar_comparacion)
+        layout.addWidget(btn_actualizar)
+
+        # Botones de ingreso de datos
+        btn_izquierda = QPushButton("Agregar Datos (Izquierda)")
+        btn_izquierda.clicked.connect(self.abrir_dialogo_izquierda)
+        layout.addWidget(btn_izquierda)
+
+        
+        btn_derecha = QPushButton("Agregar Datos (Derecha)")
+        btn_derecha.clicked.connect(self.abrir_dialogo_derecha)
+        layout.addWidget(btn_derecha)
+
+        self.setLayout(layout)
+
+    def abrir_dialogo_izquierda(self):
+        dialog = DatosDivisaDialog(self.divisa_izquierda)
+        if dialog.exec() == QDialog.DialogCode.Accepted:
+            self.actualizar_comparacion()
+
+    def abrir_dialogo_derecha(self):
+        dialog = DatosDivisaDialog(self.divisa_derecha)
+        if dialog.exec() == QDialog.DialogCode.Accepted:
+            self.actualizar_comparacion()
+
+    def actualizar_comparacion(self):
+        equivalente_izquierda = self.divisa_izquierda.calcular_equivalente_usd()
+        equivalente_derecha = self.divisa_derecha.calcular_equivalente_usd()
+
+        if equivalente_izquierda > equivalente_derecha:
+            self.label_comparacion.setText(">")
+        elif equivalente_izquierda < equivalente_derecha:
+            self.label_comparacion.setText("<")
+        else:
+            self.label_comparacion.setText("=")
+
+        self.label_izquierda.setText(f"{self.divisa_izquierda.nombre}: {equivalente_izquierda}")
+        self.label_derecha.setText(f"{self.divisa_derecha.nombre}: {equivalente_derecha}")
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    gui = GUI()
+    gui.show()
+    sys.exit(app.exec())
+
         
