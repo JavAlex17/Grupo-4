df7562c35b5d251c6b79d20be9eb28edfef5180e
JavAlex17 <72239869+JavAlex17@users.noreply.github.com>
2023-05-08 11:47:43 -0400

    Cambios

--- Taller.py
+++ Taller.py
@@ -1,29 +1,31 @@
 import sys
-import pandas as pd
-from PyQt6.QtWidgets import QApplication, QMainWindow, QWidget, QTableWidget, QTableWidgetItem, QVBoxLayout, QHBoxLayout, QPushButton
-
+from PyQt6.QtWidgets import QApplication, QMainWindow, QWidget, QLabel, QTableWidget, QTableWidgetItem, QVBoxLayout, QHBoxLayout, QPushButton, QLineEdit, QFormLayout 
 #Yen vs Dolar Can.
-#
+# 
 
 class VentanaComparacion(QMainWindow):
     def __init__(self):
         super().__init__()
         self.resize(600,500)
-        
         self.setWindowTitle("Grupo 4: Yen vs Dólar Canadience")
+        layout = QVBoxLayout()
         
+        messageL = QLabel('Bienvenido')
+        layout.addWidget(messageL)
         self.botonComparar = QPushButton('Comparar')
         self.botonComparar.clicked.connect(self.comparar)
-        layout = QVBoxLayout()
+        
         layout.addWidget(self.botonComparar)
         central_widget = QWidget()
         central_widget.setLayout(layout)
         self.setCentralWidget(central_widget)
         
+        
+        
+        
     def comparar():
         pass
         
-
     
     
 app = QApplication(sys.argv)
